a870cd97f8a78b598fb138f797c0376868d007a6
Mrtup <pinedapablo716@gmail.com>
2018-01-03 17:37:17 -0500

    ciclo FOR

--- Condicionales.js
+++ Condicionales.js
@@ -1,14 +1,4 @@
-var lina = 3
-switch (lina){
-    case 1:
-        alert("estas joven");
-        break;
-    case 2:
-        alert("estas muy joven");
-    case 3:
-        alert("joven aun");
-        break;
-    default:
-        alert("viejita");
-        break;
+var dias = ["lunes","martes","miercoles","jueves","viernes"];
+for (var i=0; i<5; i++){
+    alert(dias[3]);
 }
