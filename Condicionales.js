2b7bcb30c740b5917d82b9cbbd18c4addd5ba802
Mrtup <pinedapablo716@gmail.com>
2018-01-03 14:56:23 -0500

    Condicional if else

--- Condicionales.js
+++ Condicionales.js
@@ -1,2 +1,8 @@
 var frutas = ["mora","uva","mango","fresa"];
-document.write(frutas [0]);
+
+if(frutas[2]=="mango"){
+    document.write("esto es un mango");
+}
+else{
+    document.write("esto no aplica");
+}
