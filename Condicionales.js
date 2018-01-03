c64deb71a8fccd17a3151fb5ea996bf0b7f4aa71
Mrtup <pinedapablo716@gmail.com>
2018-01-03 15:28:28 -0500

    IF contatenado

--- Condicionales.js
+++ Condicionales.js
@@ -1,7 +1,11 @@
 // LO VAMOS A LOGRAR LO VAMOS A DOMINAR
-var pablo =25
-var lina =24
+var numero1 = 23
+var numero2 = 38
 
-if ("pablo">"lina"){
-    alert("pablo es mayor");
+if(numero1 <= numero2){
+    alert("numero " + numero1 + " es menor a " + numero2);
+}
+
+if(numero1 >= numero2){
+    alert("numero " + numero1 + " es numero menor a " + numero2);
 }
