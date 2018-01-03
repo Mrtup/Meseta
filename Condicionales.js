e3746df332ce9c12761850a4d735570a352f8791
Mrtup <pinedapablo716@gmail.com>
2018-01-03 15:37:37 -0500

    Condicional ELSE IF

--- Condicionales.js
+++ Condicionales.js
@@ -2,10 +2,10 @@
 var numero1 = 23
 var numero2 = 38
 
-if(numero1 <= numero2){
-    alert("numero " + numero1 + " es menor a " + numero2);
+if(numero1 >= numero2){
+    alert("numero " + numero1 + " es mayor a " + numero2);
 }
 
-if(numero1 >= numero2){
-    alert("numero " + numero1 + " es numero menor a " + numero2);
+else if (numero1 <= numero2){
+    alert("numero " + numero1 + " es menor a " + numero2);
 }
