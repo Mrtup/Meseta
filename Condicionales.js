85644626f5b3cd1c60e6e52e00721763e5f7153e
Mrtup <pinedapablo716@gmail.com>
2018-01-09 14:29:15 -0500

    Calculadora solo

--- Condicionales.js
+++ Condicionales.js
@@ -1,4 +1,21 @@
-var numero = 3
-if (numero > 3 || numero ==3 ){
-    document.write("Es un numero es 3")
+var num1 = prompt ("primer numero");
+var num2 = prompt ("segundo numero");
+var operacion = prompt ("operacion a realizar");
+if (operacion == "suma"){
+    // cuando es suma se debe utilizar parseInt ya que el (+) lo que hace es concatenar
+    document.write (parseInt (num1) + parseInt (num2));
+}
+
+else if (operacion == "resta"){
+    document.write (num1 - num2);
+}
+
+else if (operacion == "multi"){
+    document.write (num1 * num2);
+
+}
+
+else if (operacion == "div"){
+    document.write (num1 / num2);
+
 }
