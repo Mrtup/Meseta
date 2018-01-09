0764c445930b00c473fd1190f083781de013af3f
Mrtup <pinedapablo716@gmail.com>
2018-01-09 08:29:48 -0500

    Funcion clara con switch

--- Condicionales.js
+++ Condicionales.js
@@ -1,4 +1,12 @@
-var dias = ["lunes","martes","miercoles","jueves","viernes"];
-for (var i=0; i<5; i++){
-    alert(dias[3]);
+var casa = 3
+switch (casa){
+    case 1:
+        document.write("es la casa")
+        break;
+    case 2:
+        document.write("no es la casa")
+        break;
+    case 3:
+        document.write("ya te pasaste")
+        break;
 }
