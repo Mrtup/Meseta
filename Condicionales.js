67dac37cc2c1e2d8d566d003c4ffa4ad16faf7ef
Mrtup <pinedapablo716@gmail.com>
2018-01-09 14:54:00 -0500

    calculadora IF anidada

--- Condicionales.js
+++ Condicionales.js
@@ -1,21 +1,27 @@
 var num1 = prompt ("primer numero");
 var num2 = prompt ("segundo numero");
-var operacion = prompt ("operacion a realizar");
-if (operacion == "suma"){
-    // cuando es suma se debe utilizar parseInt ya que el (+) lo que hace es concatenar
-    document.write (parseInt (num1) + parseInt (num2));
-}
+if (!isNaN (num1) && !isNaN (num2) ) {
+    
+    var operacion = prompt ("operacion a realizar");
+    if (operacion == "suma"){
+        // cuando es suma se debe utilizar parseInt ya que el (+) lo que hace es concatenar
+        document.write (parseInt (num1) + parseInt (num2));
+    }
 
-else if (operacion == "resta"){
-    document.write (num1 - num2);
-}
+    else if (operacion == "resta"){
+        document.write (num1 - num2);
+    }
 
-else if (operacion == "multi"){
-    document.write (num1 * num2);
+    else if (operacion == "multi"){
+        document.write (num1 * num2);
 
-}
+    }
 
-else if (operacion == "div"){
-    document.write (num1 / num2);
+    else if (operacion == "divi"){
+        document.write (num1 / num2);
 
-}
+    }
+
+    }
+else 
+    alert ("No es una operacion");
