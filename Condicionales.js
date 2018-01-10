b6739f33f6ebad4e9a673a2f088722f3b0bdb631
Mrtup <pinedapablo716@gmail.com>
2018-01-10 08:53:11 -0500

    Primer ejemplo ciclo While

--- Condicionales.js
+++ Condicionales.js
@@ -1,27 +1,7 @@
-var num1 = prompt ("primer numero");
-var num2 = prompt ("segundo numero");
-if (!isNaN (num1) && !isNaN (num2) ) {
-    
-    var operacion = prompt ("operacion a realizar");
-    if (operacion == "suma"){
-        // cuando es suma se debe utilizar parseInt ya que el (+) lo que hace es concatenar
-        document.write (parseInt (num1) + parseInt (num2));
-    }
+var meses = ["enero","febrero","marzo","abril","mayo","junio","julio"];
+var contador = 0;
 
-    else if (operacion == "resta"){
-        document.write (num1 - num2);
-    }
-
-    else if (operacion == "multi"){
-        document.write (num1 * num2);
-
-    }
-
-    else if (operacion == "divi"){
-        document.write (num1 / num2);
-
-    }
-
-    }
-else 
-    alert ("No es una operacion");
+while (contador<meses.length){
+    alert(meses[contador]);
+    contador++;
+}
