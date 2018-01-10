a7728c1954be7873957a30bd0cade9a259079803
Mrtup <pinedapablo716@gmail.com>
2018-01-10 09:31:29 -0500

    formulario con IF y WHILE

--- Condicionales.js
+++ Condicionales.js
@@ -1,7 +1,13 @@
-var meses = ["enero","febrero","marzo","abril","mayo","junio","julio"];
-var contador = 0;
-
-while (contador<meses.length){
-    alert(meses[contador]);
-    contador++;
+var nombre, edad, sexo;
+nombre = prompt ("introduce nombre");
+edad = prompt ("introduce edad");
+if (edad >= 90){
+   alert ("has introducido edad incorrecta");
+    while (edad>=90){
+        edad = prompt ("introduce edad");
+        alert ("has introducido edad incorrecta");
 }
+}
+sexo = prompt ("introduce sexo");
+
+document.write(nombre + "<br/>",edad + "<br/>", sexo)
